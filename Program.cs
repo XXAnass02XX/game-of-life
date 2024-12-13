fda42a8e91930849186297a339328689446cd866
haydara <anass.haydar@grenoble-inp.org>
2024-12-13 11:26:38 +0100

    gameoflife working

--- Program.cs
+++ Program.cs
@@ -8,58 +8,69 @@ class Program
         static void Main(string[] args)
         {
             // Read the file contents
-            string[] lines = File.ReadAllLines("examples/example1.txt");
+            Console.WriteLine("which example you want to run , type just a number from 1 to 4 :");
+            int nbr = int.Parse(Console.ReadLine());
+            try
+            {
+                string[] lines = File.ReadAllLines($"examples/example{nbr}.txt");
 
             // Create the initial grid
-            Console.WriteLine($"the lens are : {lines[0].Length} and {lines.Length}");
-            GridA our_grid = new GridA(lines[0].Length, lines.Length);
-            for (int i = 0; i < lines.Length; i++)
-            {
-                for (int j = 0; j < lines[i].Length; j++)
+                Console.WriteLine($"the lens are : {lines[0].Length} and {lines.Length}");
+                GridA our_grid = new GridA(lines[0].Length, lines.Length);
+                for (int i = 0; i < lines.Length; i++)
                 {
-                    try
+                    for (int j = 0; j < lines[i].Length; j++)
                     {
-                        our_grid.grid[i][j].state = lines[i][j] == '1' ? 1 : 0;
+                        try
+                        {
+                            our_grid.grid[i][j].state = lines[i][j] == '1' ? 1 : 0;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"An error occurred: {ex.Message}");
+                            Console.WriteLine($"problem at :::: i: {i} , j : {j}");
+                            return ;
+                        }
+                        
                     }
-                    catch (Exception ex)
+                }
+                /* to see the created grid
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    for (int j = 0; j < lines[i].Length; j++)
                     {
-                        Console.WriteLine($"An error occurred: {ex.Message}");
-                        Console.WriteLine($"problem at :::: i: {i} , j : {j}");
-                        return ;
+                        Console.Write(our_grid.grid[i][j].state);
                     }
-                    
+                    Console.Write("\n");
+                }
+                return ;*/
+                // Run the Game of Life simulation
+                int iter = 0;
+                while (iter < 10)
+                {
+                    our_grid.Update();
+                    PrintGrid(our_grid);
+                    System.Threading.Thread.Sleep(500); // Pause for 0.5 seconds
+                    iter ++;
                 }
             }
-            /* to see the created grid
-            for (int i = 0; i < lines.Length; i++)
+            catch (Exception ex)
             {
-                for (int j = 0; j < lines[i].Length; j++)
-                {
-                    Console.Write(our_grid.grid[i][j].state);
-                }
-                Console.Write("\n");
-            }
-            return ;*/
-            // Run the Game of Life simulation
-            int iter = 0;
-            while (iter < 3)
-            {
-                our_grid.Update();
-                PrintGrid(our_grid);
-                System.Threading.Thread.Sleep(500); // Pause for 0.5 seconds
-                iter ++;
+                Console.WriteLine($"an error occured : {ex}");
             }
         }
 
         static void PrintGrid(GridA our_grid)
         {
-            //Console.Clear();
+            Console.Clear();
             Console.WriteLine("state------");
+            string square ;
             for (int i = 0; i < our_grid.grid.Count; i++)
             {
                 for (int j = 0; j < our_grid.grid[i].Count; j++)
                 {
-                    Console.Write(our_grid.grid[i][j].state);
+                    square = our_grid.grid[i][j].state == 0 ? "█" : " " ; 
+                    Console.Write(square);
                 }
                 Console.WriteLine();
             }
